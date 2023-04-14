9f0e24adb42abc5c456dda0d0ed62c41315a777d
ASHISH_ANAND <ashishanand2001@gmial.com>
2023-04-15 00:02:23 +0530

    page3_complete

--- final_project1/script.js
+++ final_project1/script.js
@@ -45,6 +45,9 @@ function init(){
 init()
 var cursor = document.querySelector("#cursor")
 var overlay = document.querySelector("#overlay")
+var p3img = document.querySelector("#p3img")
+var overlayp3 = document.querySelector("#overlayp3")
+
 
 overlay.addEventListener("mouseenter",function(){
     cursor.style.scale = "1"
@@ -57,13 +60,94 @@ overlay.addEventListener("mousemove",function(dets){
     cursor.style.top = dets.y - 40+ "px" 
 })
 gsap.to("#image1",{
-    rotate:"7deg",
+    rotate:"4deg",
     scrollTrigger:{
         trigger:"#image1",
         scroller:"#main",
         scrub:3,
         // markers:true,
         start:"top 70%",
+        end:"top -30%",
         // pin:true
     }
+})
+
+gsap.to("svg",{
+  top:"5%",
+  scale:.8,
+  fill: "#111",
+  scrollTrigger:{
+    trigger:"svg",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top 45%",
+    end:"top -50%"
+  }
+})
+gsap.to("svg",{
+  fill: "#fff",
+  scrollTrigger:{
+    trigger:"svg",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top -223%",
+    // end:"top -210%"
+  }
+})
+gsap.to("#nav",{
+  background:" linear-gradient(#00000052,#0000000b,#00000000)",
+  scrollTrigger:{
+    trigger:"#nav",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top -223%",
+    // end:"top -210%"
+  }
+})
+gsap.to("#nav h1",{
+  color: "#fff",
+  scrollTrigger:{
+    trigger:"#nav h1",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top -267%",
+    // end:"top -210%"
+  }
+})
+
+gsap.to("#nav h1",{
+  color:"#111",
+  scrollTrigger:{
+    trigger:"svg",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top -20%",
+    end:"top -50%"
+  }
+})
+gsap.to("#nav",{
+  background:" linear-gradient(#ffffff52,#ffffff0b,#ffffff00)",
+  scrollTrigger:{
+    trigger:"svg",
+    scroller:"#main",
+    scrub:true,
+    // markers:true,
+    start:"top -20%",
+    end:"top -50%"
+  }
+})
+
+overlayp3.addEventListener("mousemove",(dets)=>{
+  p3img.style.opacity = 1
+  p3img.style.left = dets.x + "px"
+  p3img.style.top = dets.y + "px"
+
+})
+overlayp3.addEventListener("mouseleave",()=>{
+  p3img.style.opacity = 0
 })
